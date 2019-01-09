fc8ce31a784c308823180c98591e6fd5af959066
Jayanta Debnath <jayanta.dn@gmail.com>
2019-01-09 08:38:49 +0530

    compressing multiple files

--- Compress.sh
+++ Compress.sh
@@ -1,10 +1,30 @@
+echo "Setting global variables"
 ZIP="T:/ProgramFiles/7-ZipPortable/App/7-Zip64/7zG.exe"
 
+echo "Checking parameters"
 [ $# -lt 2 ] && echo "ERROR: insufficient parameters" && exit
 
+echo "Processing the arguments"
 command=$1
-
 shift
 
-$ZIP a -tzip -mx9 "$1.zip" $*
+echo "Setting the compressed filename"
+if [ $# -eq 1 ] 
+then
+	TARGET="$1.zip"
+else
+	dir=`echo "$1" | awk 'BEGIN { FS="\\\\" } { print $(NF-1) }'`
+	TARGET=`echo "$1" | awk 'BEGIN { FS="\\\\"; ORS="\\\\" } { for(i=1; i<NF; i++) print $i}'`
+	TARGET=${TARGET}${dir}.zip
+fi
 
+echo -n "Compressing..."
+$ZIP a -tzip -mx9 "$TARGET" $*
+if [ $? -eq 0 ]
+then
+	echo "OK"
+else
+	echo "FAILED"
+	read
+	exit
+fi
