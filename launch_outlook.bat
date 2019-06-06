109eb72929df770f89801bafc65388f99a070b17
Jayanta Debnath <jayanta.dn@gmail.com>
2019-06-06 09:02:42 +0530

    outlook changes

--- launch_outlook.bat
+++ launch_outlook.bat
@@ -1,3 +1,7 @@
 @echo off
 
-"C:\Program Files (x86)\Microsoft Office\root\Office16\OUTLOOK.EXE" /a %1
+echo "extract the filename from the path"
+for /d %%g in (%1) do set filename=%%~ng%%~xg
+
+echo "Launching outlook..."
+"C:\Program Files\WindowsApps\Microsoft.Office.Desktop.Outlook_16051.11629.20196.0_x86__8wekyb3d8bbwe\Office16\OUTLOOK.EXE" /c ipm.note /m "&subject=%filename%" /a %1
