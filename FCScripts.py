dccc4996edb1664fcdd2883fe9167b1e27ceee49
Jayanta Debnath <jayanta.dn@gmail.com>
2021-05-31 20:57:14 +0530

    BackupDir

--- FCScripts.py
+++ FCScripts.py
@@ -1,10 +1,13 @@
 # Configuration
 ZIP = "T:\\ProgramFiles\\7-ZipPortable\\App\\7-Zip64\\7zG.exe"
+BUPDIR = "X:\\Backup"
 
 # All imports
 import os
 import sys
 import subprocess
+import time
+import shutil
 
 # function definitions
 def myassert(expr, msg) :
@@ -17,7 +20,7 @@ def myassert(expr, msg) :
 def Compress(filelist) :
     # compute the output file name
     if len(filelist) == 1 :
-        outfile = filelist[0] + ".zip"
+        outfile = filelist[0].rstrip("\\") + ".zip"
     elif len(filelist) > 1 :
         dirname = os.path.dirname( filelist[0] )
         if not dirname :
@@ -30,7 +33,7 @@ def Compress(filelist) :
     command = [ ZIP, "a", "-tzip", "-mx9",  outfile ]
     for file in filelist :
         myassert( os.path.exists(file), "File does not exist: " + file )
-        command.append( file )
+        command.append( file.rstrip("\\") )
     try :
         ret = subprocess.call( command )
         myassert(ret == 0, "Failed to execute 7z command")
@@ -71,13 +74,16 @@ def hasMultiFiles(zipfile) :
         linenum += 1
     return False
        
+       
 # program entry point
 if __name__ == "__main__" :
     # doing some common sanity checks
     myassert( os.path.exists(ZIP), "7z path invalid" )
+    myassert( os.path.isdir(BUPDIR), "Backup folder is invalid" )
+    myassert(len(sys.argv) >= 2, "Program called without any commands")
 
     if sys.argv[1] == "Compress" :
-        myassert(len(sys.argv) > 1, "Insufficient parameters")
+        myassert(len(sys.argv) > 2, "Insufficient parameters")
         sys.argv.pop(0)
         sys.argv.pop(0)
         for file in sys.argv :
@@ -89,5 +95,18 @@ if __name__ == "__main__" :
         myassert( os.path.exists(sys.argv[2]), "Zip file does not exist" )
         Uncompress(sys.argv[2])
         
+    elif sys.argv[1] == "BackupDir" :
+        # Compress the given file(s)
+        myassert(len(sys.argv) > 2, "Insufficient parameters")
+        sys.argv.pop(0)
+        sys.argv.pop(0)
+        for file in sys.argv :
+            myassert(os.path.exists(file), "Invalid file: " + file)
+        zipfile = Compress(sys.argv)
+        
+        # generate timestamp and move to backup folder
+        timestamp = time.strftime("%Y%m%d_%H%M%S_", time.localtime())
+        shutil.move( zipfile, os.path.join(BUPDIR, timestamp + os.path.basename(zipfile)) )
+    
     else :
         myassert(False, "Invalid command")
